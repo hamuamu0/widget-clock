aab3b31b4b6c2ea0cd65c562b811573c7a0919c6
hamuamu0 <414179499@qq.com>
2020-11-22 02:55:18 +0800

    桌面时钟widget

---
+++ app/src/main/java/com/soul/myapplication12/hayukleung/appwidget/NullActivity.java
@@ -0,0 +1,17 @@
+package com.soul.myapplication12.hayukleung.appwidget;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * AppWidget
+ * com.hayukleung.appwidget
+ * NullActivity.java
+ *
+ * by hayukleung
+ * at 2017-03-16 18:38
+ */
+
+public class NullActivity extends AppCompatActivity {
+}
